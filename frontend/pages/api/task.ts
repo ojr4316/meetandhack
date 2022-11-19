b22c418708db8e62db8ed16cb2e4a33f62db5f31
Grahith Movva <89426729+GrahithMovva@users.noreply.github.com>
2022-11-19 17:01:45 -0500

    made some changes to tasks

--- frontend/pages/api/task.ts
+++ frontend/pages/api/task.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { PrismaClient, task, user_task } from '@prisma/client'
+
 const prisma = new PrismaClient();
 
 type Data = {
@@ -19,25 +20,37 @@ export default async function handler(
   ){
     const {name,project_name} = req.body;
     const date = new Date()
-    if(req.method == "POST" && name && project_name){
-        const project_id = (await prisma.project.findFirst({where:{name: project_name as string}}))?.id
-        const task = await prisma.task.create({data:{name: name as string, creation_date: date , finish_date: new Date("2040-12-12"), project : project_id as number}});
-        return res.status(200).json({error: Error.None, task });
-    }else if( req.method == "DELETE" && name){
-        const id = (await prisma.task.findFirst({where:{name:name as string}}))?.id
-        await prisma.task.delete({where:{id:id}})
-        return res.status(200).json({error: Error.None });
-        
-    }else if(req.method == "PUT" && name && project_name && name){
-        const id = (await prisma.task.findFirst({where:{name:name as string}}))?.id
-        const project_id = (await prisma.project.findFirst({where:{name: project_name as string}}))?.id
-        await prisma.task.update({where:{id:id},data:{name: name as string, project:project_id}});
-        return res.status(200).json({error: Error.None});
+    // const user = req.session.user
+    // if (user){
+        // if(!(await prisma.user.findFirst({where:{id: user.id}}))?.is_manager){
+            if(req.method == "POST"){
+                const project_id = (await prisma.project.findFirst({where:{name: project_name as string}}))?.id
+                await prisma.task.create({data:{name: name as string, creation_date: date , finish_date: new Date("2040-12-12"), project : project_id as number}});
+                return res.status(200).json({error: Error.None});
+            }else if( req.method == "DELETE" && name){
+                
+                const project_id = (await prisma.project.findFirst({where:{name: project_name as string}}))?.id
+                const id = (await prisma.task.findFirst({where:{name:name as string,project : project_id}}))?.id
+                await prisma.task.delete({where:{id:id}})
+                return res.status(200).json({error: Error.None });
+                
+            }else if(req.method == "PUT" && name && project_name && name){
+                const id = (await prisma.task.findFirst({where:{name:name as string}}))?.id
+                const project_id = (await prisma.project.findFirst({where:{name: project_name as string}}))?.id
+                await prisma.task.update({where:{id:id},data:{name: name as string, project:project_id}});
+                return res.status(200).json({error: Error.None});
+            }
+            else {
+                return res.status(400).json({error: Error.InvalidRequest});
+            }
+        // }
+        // else{
+        //     return res.status(400).json({error: Error.InvalidUser})
+        // }
     }
-    else {
-        return res.status(400).json({error: Error.InvalidRequest});
-      }
-  }
+
+    
+//   }
 
 
 
--- frontend/pages/task.tsx
+++ frontend/pages/task.tsx
@@ -9,6 +9,14 @@ function createtask(name:string, project:string){
     });
 }
 
+function deletetask(name:string,project:string){
+    axios({url: '/api/task',data:{name,project}, method:"DELETE"}).then((res) => {
+        console.log(res);
+    }).catch((err) => {
+        console.log(err.response.data);
+    });
+}
+
 export default function Task() {
     const [name, setName] = useState("");
     const [project,setProject] = useState("");
@@ -20,7 +28,17 @@ export default function Task() {
             <label>Project name</label>
             <input type="text" onChange={(e) => setProject(e.currentTarget.value)} value={project} />
             <button type="submit" onClick={() => createtask(name,project)}>Submit</button>
+            <div>
+            <h1>Delete Task</h1>
+            <label htmlFor="first">Task Name</label>
+            <input type="text" onChange={(e) => setName(e.currentTarget.value)} value={name} />
+            <label>Project name</label>
+            <input type="text" onChange={(e) => setProject(e.currentTarget.value)} value={project} />
+            <button onClick={() => deletetask(name,project)}>delete</button>
+
+            </div>
         </div>
+        
     )
 
 }
