5f91010c5bf2804429c1a29a11b6a5ccaa9466bb
Owen <ojr4316@rit.edu>
2022-11-19 14:09:39 -0500

    login and register api routes

---
+++ frontend/pages/api/login.ts
@@ -0,0 +1,35 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+type Data = {
+    error: Error;
+}
+
+enum Error {
+    None = "",
+    InvalidUsername = "Could not find username",
+    InvalidPassword = "Incorrect Password"
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const { email, password } = req.body; 
+
+  const userExists = await prisma.user.findFirst({where: {email: email as string}});
+
+  if (userExists) {
+    const correctPassword = bcrypt.compareSync(password, userExists.password);
+    if (correctPassword) {
+      return res.status(200).json({error: Error.None });
+    } else {
+      return res.status(400).json({error: Error.InvalidPassword });
+    }
+  } else {
+    return res.status(400).json({error: Error.InvalidUsername});
+  }
+}
--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -1,17 +1,32 @@
-import { Component, useState } from "react"
-export default function Login(){
+import { Component, useState } from "react";
+import axios from "axios";
+
+function login(username: string, password: string) {
+  axios({url: "/api/login", data: {username, password}}).then((res) => {
+    console.log(res);
+  });
+}
+
+export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-    return(
-        <form name="form1" action="/api/form" method="post">
-          <h1>Login</h1>
-          <label htmlFor="last">Email</label>
-          <input type="text" onChange={(e) => setEmail(e.currentTarget.value)} value={email}/>
-          <label htmlFor="last">Password</label>
-          <input type="text" onChange={(e) => setPassword(e.currentTarget.value)} value={password}/>
-          <button type="submit">Sign In</button>
-        </form>
-    )
-    
+  return (
+    <form name="form1" action="/api/form" method="post">
+      <h1>Login</h1>
+      <label htmlFor="last">Email</label>
+      <input
+        type="text"
+        onChange={(e) => setEmail(e.currentTarget.value)}
+        value={email}
+      />
+      <label htmlFor="last">Password</label>
+      <input
+        type="text"
+        onChange={(e) => setPassword(e.currentTarget.value)}
+        value={password}
+      />
+      <button type="submit" onClick={() => login(email, password)}>Sign In</button>
+    </form>
+  );
 }
