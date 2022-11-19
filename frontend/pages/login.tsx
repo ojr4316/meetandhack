63e9f40c70b375a61e1b7645c26d4c94d32e25d2
dmp3872 <dmp3872@rit.edu>
2022-11-19 13:55:05 -0500

    states for attributes

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -1,10 +1,15 @@
+import { Component, useState } from "react"
 export default function Login(){
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
     return(
         <form name="form1" action="/api/form" method="post">
+          <h1>Login</h1>
           <label htmlFor="last">Email</label>
-          <input type="text" id="email" name="email" required />
+          <input type="text" onChange={(e) => setEmail(e.currentTarget.value)} value={email}/>
           <label htmlFor="last">Password</label>
-          <input type="text" id="last" name="last" required />
+          <input type="text" onChange={(e) => setPassword(e.currentTarget.value)} value={password}/>
           <button type="submit">Sign In</button>
         </form>
     )
--- frontend/pages/register.tsx
+++ frontend/pages/register.tsx
@@ -1,15 +1,21 @@
+import { useState } from "react";
+
 export default function Register() {
+    const [email, setEmail] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [password, setPassword] = useState("");
     return (
         <form name="form1" action="/api/form" method="post">
             <h1>Register</h1>
             <label htmlFor="email">Email</label>
-            <input type="text" id="email" name="email" required />
-            <label htmlFor="first">First</label>
-            <input type="text" id="first" name="first" />
+            <input type="text" onChange={(e) => setEmail(e.currentTarget.value)} value={email}/>
+            <label htmlFor="first">First Name</label>
+            <input type="text" onChange={(e) => setFirstName(e.currentTarget.value)} value={firstName}/>
             <label htmlFor="last">Last Name</label>
-            <input type="text" id="last" name="last" required />
+            <input type="text" onChange={(e) => setLastName(e.currentTarget.value)} value={lastName}/>
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" name="password" required />
+            <input type="password" onChange={(e) => setPassword(e.currentTarget.value)} value={password}/>
             <button type="submit">Submit</button>
         </form>
     )
